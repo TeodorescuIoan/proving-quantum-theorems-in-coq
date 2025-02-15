35622108cf7de091fa7cfe27c6efb6ca705fbd72
TeodorescuIoan <ioanoctavian01@gmail.com>
2025-02-15 23:00:17 +0200

    Simplify phase kickback proof

--- theories/PhaseKickback.v
+++ theories/PhaseKickback.v
@@ -1,13 +1,15 @@
 From QuantumLib Require Import Quantum.
 
+(*
+  A CNOT gate operates as follows:
 
+   |Φ⟩ ---|--- |Φ⟩  
+   |Ψ⟩ ---⊕--- |Ψ⟩ ⊕ |Φ⟩ 
+
+  However, surrounding it by Hadamard
+  gates will reverse the control bit:
+*)
 Lemma phase_kickback_cnot : 
   (hadamard ⊗ hadamard) × cnot × (hadamard ⊗ hadamard) = notc.
-Proof. 
-  prep_matrix_equivalence.
-  unfold kron, hadamard, cnot, notc, Mmult; simpl.
-  assert (H: C2 * (/ C2 * / C2) + / C2 * / C2 + / C2 * / C2 = C1)
-    by (rewrite !Cmult_assoc; lca). 
-  by_cell; try lca; autorewrite with C_db; apply H.
-Qed.
+Proof. crunch_matrix; lca. Qed.
 
